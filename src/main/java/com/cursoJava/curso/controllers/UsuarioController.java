c4203d59053cac253a7c59a22be6f61420de655b
roopyx <roopyx.1111@gmail.com>
2022-03-04 16:37:21 -0300

    get Users and show in table

--- src/main/java/com/cursoJava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursoJava/curso/controllers/UsuarioController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "Usuario/{id}")
+    @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -19,6 +21,38 @@ public class UsuarioController {
         usuario.setEmail("Lazlo.cali@gmail.com");
         usuario.setTelefono("+56948376593");
         return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(22121L);
+        usuario.setNombre("Lazlo");
+        usuario.setApellido("California");
+        usuario.setEmail("Lazlo.cali@gmail.com");
+        usuario.setTelefono("+56948376593");
+
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(212121L);
+        usuario1.setNombre("Lazlo1");
+        usuario1.setApellido("California1");
+        usuario1.setEmail("Lazlo.1@gmail.com");
+        usuario1.setTelefono("+56943236593");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(1344534L);
+        usuario2.setNombre("Lazlo2");
+        usuario2.setApellido("California2");
+        usuario2.setEmail("Lazlo.2@gmail.com");
+        usuario2.setTelefono("+56942276593");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario1);
+        usuarios.add(usuario2);
+
+        return usuarios;
     }
 
 }
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,25 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  cargarUsuarios()
+  $('#usuarios').DataTable();
+});
+
+async function cargarUsuarios() {
+    const request = await fetch('usuarios', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+    })
+
+    const usuarios = await request.json()
+
+    let listadoHTML = ''
+    for (let usuario of usuarios) {
+      let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>'+usuario.nombre+ ' ' +usuario.apellido+'</td><td>'+usuario.email+'</td><td>'+usuario.telefono+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>'
+      listadoHTML += usuarioHtml
+    }
+
+    document.querySelector('#usuarios tbody').outerHTML = listadoHTML
+}
